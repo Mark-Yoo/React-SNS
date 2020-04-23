bb085378b966f9b703aa7455fcafbf2bff5eced5
Mark Yoo <yjinhyuk@gmail.com>
2020-04-23 14:44:40 +0900

    Add: RegExp and error messages are now added to signup page

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React-SNS
+
+목적: React를 활용하여 SNS의 기본 기능 구축해보기
+
+
+
+
+
+진행 중 만난 오류
+- 전날까지 잘 실행되던 npm run dev에서 아래와 같은 에러코드가 발생
+- next 관련 코드를 건드리지 않았으나 git에 원인을 알 수 없는 에러로 보고되어 있었으며, 어떤 사람은 webpack과 @babel/core를 업데이트 하여 해결되었다는 글이 있었으나 본인의 경우에는 해결되지 않음
+- 결국 node_module 폴더와 package-lock.json 파일을 삭제 후 npm i로 다시 dependency에 등록된 패키지들을 다시 설치하고 해결됨.
+
+```
+./node_modules/next/dist/client/next-dev.js 36:6
+Module parse failed: Unexpected token (36:6)
+You may need an appropriate loader to handle this file type.
+| 
+| 
+> import('./noop'); // Support EventSource on Internet Explorer 11
+```
--- node-bird/ch1/front/pages/signup.js
+++ node-bird/ch1/front/pages/signup.js
@@ -9,23 +9,55 @@ const Signup = () => {
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
   const [term, setTerm] = useState(false);
+  const [idError, setIdError] = useState(false);
+  const [pwError, setPwError] = useState(false);
+  const [pwckError, setPwckError] = useState(false);
+  const [termError, setTermError] = useState(false);
+  const idRegExp = new RegExp(/^[a-zA-Z0-9+]{5,13}$/g);
+  const pwRegExp = new RegExp(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/);
 
+  // 아이디
   const onChangeId = (e) => {
     setId(e.target.value);
   };
+
+  const onCheckId = () => {
+    idRegExp.test(id) ? setIdError(false) : setIdError(true);
+  }
+
+  // 닉네임
   const onChangeNick = (e) => {
     setNick(e.target.value);
   };
+
+  // 패스워드
   const onChangePw = (e) => {
     setPassword(e.target.value);
   };
+
+  const onCheckPw = () => {
+    pwRegExp.test(password) ? setPwError(false) : setPwError(true);
+  }
+
+  // 패스워드 체크
   const onChangePwck = (e) => {
     setPasswordCheck(e.target.value);
   };
+
+  const onCheckpwck = () => {
+    passwordCheck === password ? setPwckError(false) : setPwckError(true);
+  }
+
+  // 약관 체크
   const onChangeTerm = (e) => {
-    setTerm(e.target.value);
+    setTerm(e.target.checked);
   };
-  const onSubmit = () => {};
+
+  // 폼 제출
+  const onSubmit = (e) => {
+    e.preventDefault();
+    term ? setTermError(false) : setTermError(true);
+  };
 
   return(
     <>
@@ -34,29 +66,32 @@ const Signup = () => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.1.4/antd.css" />
       </Head>
       <AppLayout>
-          <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
           <div>
             <label htmlFor="user-id">아이디</label>
             <br />
-            <Input name ="user-id" required value={id}onChange={onChangeId} />
+            <Input name ="user-id" required value={id} onChange={onChangeId} onBlur={onCheckId} placeholder="아이디는 영문 대소문자와 숫자의 조합 5~13글자로 만들어주세요."/>
+            {idError ? <div style={{color: 'red'}}>아이디의 형식이 맞지 않습니다.</div> : ''}
           </div>
           <div>
             <label htmlFor="user-nickname">닉네임</label>
             <br />
-            <Input name ="user-nickname" required value={nick} onChange={onChangeNick} />
+            <Input name ="user-nickname" required value={nick} onChange={onChangeNick} placeholder="닉네임은 영문 대소문자, 한글, 숫자의 조합으로 만들어주세요."/>
           </div>
           <div>
             <label htmlFor="user-password">패스워드</label>
             <br />
-            <Input name ="user-password" type="password" required value={password} onChange={onChangePw} />
+            <Input name ="user-password" type="password" required value={password} onChange={onChangePw} onBlur={onCheckPw} placeholder="8~16자리의 영문 대소문자, 특수문자, 숫자의 조합으로 만들어주세요" />
           </div>
           <div>
-            <label htmlFor="user-password-ck">패스워드</label>
+            <label htmlFor="user-password-ck">패스워드 확인</label>
             <br />
-            <Input name ="user-password-ck" type="password" required value={passwordCheck} onChange={onChangePwck} />
+            <Input name ="user-password-ck" type="password" required value={passwordCheck} onChange={onChangePwck} onBlur={onCheckpwck} placeholder="비밀번호를 다시 입력하여 주세요" />
+            {pwckError ? <div style={{color: 'red'}}>비밀번호가 일치하지 않습니다.</div> : ''}
           </div>
           <div>
-            <Checkbox name="user-term" value={term} onChange={onChangeTerm}>약관에 동의합니다.</Checkbox>
+            <Checkbox name="user-term" onChange={onChangeTerm}>약관에 동의합니다.</Checkbox>
+            {termError ? <div style={{color: 'red'}}>약관에 동의하여주세요</div> : ''}
           </div>
           <div>
             <Button type="primary" htmlType="submit">가입하기</Button>
