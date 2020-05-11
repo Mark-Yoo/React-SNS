d86676901544341ae474bc13eb0a11f462054392
Mark Yoo <yjinhyuk@gmail.com>
2020-05-11 17:26:47 +0900

    Modified: prop-types added for type check

--- node-bird/ch1/front/pages/_app.js
+++ node-bird/ch1/front/pages/_app.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Head from "next/head";
+import PropTypes from "prop-types";
 import AppLayout from "../components/AppLayout";
 
 // Component는 next에서 전달해주는 props로 각 컴포넌트들을 전달한다.
 // Component는 JSX 문법으로 사용해야 한다.
 // _app.js는 기본적인 레이아웃으로 인식한다.
+// prop-types를 이용해 props를 검증해주는 기능을 react가 지원한다.
 const NodeBird = ({ Component }) => {
   return (
     <>
@@ -20,6 +22,11 @@ const NodeBird = ({ Component }) => {
       </AppLayout>
     </>
   );
+};
+
+// node 타입은 렌더링이 가능한 모든 것을 포함한다.
+NodeBird.propTypes = {
+  Component: PropTypes.elementType,
 };
 
 export default NodeBird;
