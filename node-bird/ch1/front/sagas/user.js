7735f6404c5cf4bdd1e441f782afe83014015ab5
Mark Yoo <yjinhyuk@gmail.com>
2020-05-21 17:18:52 +0900

    Modified: Readme.md - redux-saga added Modified: sagas/user.js basic code added

--- node-bird/ch1/front/sagas/user.js
+++ node-bird/ch1/front/sagas/user.js
@@ -1,15 +1,19 @@
 import { all } from "redux-saga/effects";
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "../reducers/user";
 
-function loginAPI() {}
+function loginAPI() {
+  // 서버에 요청을 보내는 부분
+}
 
 function* login() {
   try {
     yield call(loginAPI);
     yield put({
+      // put은 dispatch와 동일
       type: LOG_IN_SUCCESS,
     });
   } catch (e) {
+    // loginAPI 실패
     console.error(e);
     yield put({
       type: LOG_IN_FAILURE,
@@ -18,6 +22,7 @@ function* login() {
 }
 
 function* watchLogin() {
+  // 로그인 액션이 실행되면 login 함수를 실행
   yield takeLatest(LOG_IN, login);
 }
 
